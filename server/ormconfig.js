4ccbb19737b331b0331f178143fb88b75d842e7d
Valentin <47883020+val77310@users.noreply.github.com>
2025-08-01 14:30:29 +0200

    Delete server directory

--- server/ormconfig.js
+++
@@ -1,6 +0,0 @@
-import pkg from 'pg';
-const { Pool } = pkg;
-
-export default new Pool({
-  connectionString: process.env.DATABASE_URL || `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@db:5432/poopmap`
-});
